b64fb77f75d3162a5bcda4853ec3bd463715dc9e
Mash1-1 <mahderashenafi68@gmail.com>
2025-07-14 15:16:30 +0300

    Did Task2

---
+++ Introduction_to_go/task2.go
@@ -0,0 +1,54 @@
+// Task 2
+// Palindrome check
+// Write a Go function that takes a string as input and checks whether it is a palindrome or not.
+// A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward
+// (ignoring spaces, punctuation, and capitalization).
+// [Optional]: Write test for your function
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func freq_count(s string) map[string]int {
+	mp := make(map[string]int)
+
+	prev := ""
+
+	for i := 0; i < len(s); i++ {
+		if s[i] != ' ' {
+			prev += string(s[i])
+		}else {
+			prev = strings.ToLower(prev)
+			mp[prev] += 1
+			prev = ""
+		}
+	}
+	mp[prev] += 1
+	return mp
+}
+
+func IsPalindrome(s string) bool {
+	left := 0 
+	right := len(s) - 1
+
+	for left <= right {
+		if s[left] != s[right] {
+			return false
+		}
+		left += 1
+		right -= 1
+	}
+	return true
+}
+
+func main() {
+	greeting := "Hello World world"
+	fmt.Println(freq_count(greeting))
+
+	t1 := "aba"
+	t2 := "ab"
+	fmt.Printf("Test 1: %v\nTest 2: %v\n", IsPalindrome(t1), IsPalindrome(t2)) 
+}
