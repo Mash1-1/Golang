1c2104cb68e683e0a2bb361f484d528953c42905
Mahder Ashenafi <mahderashenafi68@gmail.com>
2025-07-14 15:17:57 +0300

    Update task2.go
    Removed commented question statement

--- Introduction_to_go/task2.go
+++ Introduction_to_go/task2.go
@@ -1,10 +1,4 @@
 // Task 2
-// Palindrome check
-// Write a Go function that takes a string as input and checks whether it is a palindrome or not.
-// A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward
-// (ignoring spaces, punctuation, and capitalization).
-// [Optional]: Write test for your function
-
 package main
 
 import (
